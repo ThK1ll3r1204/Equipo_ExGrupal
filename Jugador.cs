9458809e84ae971d4560c3effcba7cc9e78b86be
César Cadillo <gkktemporal@gmail.com>
2023-10-06 20:54:26 -0500

    Clase program y clase Jugador
    El constructor recibe la creación del jugador.

---
+++ Jugador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenGrupal_AventuraDeTexto
+{
+    class Jugador
+    {        
+        public string Nombre { get; set; }
+        public int Fuerza { get; set; }
+        public int Destreza { get; set; }
+        public int Vida { get; set; }
+
+        public Jugador(string nombre, int fuerza, int destreza, int vida)
+        {
+            Nombre = nombre;
+            Fuerza = fuerza;
+            Destreza = destreza;
+            Vida = vida;
+        }
+
+        public void MostrarEstado()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Fuerza: {Fuerza}");
+            Console.WriteLine($"Destreza: {Destreza}");
+            Console.WriteLine($"Vida: {Vida}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace ExamenGrupal_AventuraDeTexto
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bienvenido a la aventura de texto");
+
+            Console.Write("Ingresa el nombre de tu jugador: ");
+            string nombre = Console.ReadLine();
+
+            int fuerza, destreza, vida;
+
+            do
+            {
+                Console.WriteLine("Tu fuerza, destreza y vida juntas no pueden sumar más de 100 puntios");
+                Console.Write("Asigna puntos de fuerza (max 100): ");
+                fuerza = int.Parse(Console.ReadLine());
+
+                Console.Write("Asigna puntos de destreza (max 100): ");
+                destreza = int.Parse(Console.ReadLine());
+
+                Console.Write("Asigna puntos de vida (max 100): ");
+                vida = int.Parse(Console.ReadLine());
+
+                if (fuerza + destreza + vida > 100)
+                {
+                    Console.WriteLine("La suma de fuerza, destreza y vida no debe ser mayor que 100. Inténtalo de nuevo.");
+                }
+            } while (fuerza + destreza + vida > 100);
+
+            Jugador jugador = new Jugador(nombre, fuerza, destreza, vida);
         }
     }
 }
